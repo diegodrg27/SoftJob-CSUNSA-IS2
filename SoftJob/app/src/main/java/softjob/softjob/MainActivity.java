3070bd4656b53fba8f5beb9194875fe3a17020d5
Unknown <diegodrg27@gmail.com>
2017-12-16 20:07:33 +0100

    main menu

--- SoftJob/app/src/main/java/softjob/softjob/MainActivity.java
+++ SoftJob/app/src/main/java/softjob/softjob/MainActivity.java
@@ -178,22 +178,20 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_datosPersonales) {
             Intent intent = new Intent(MainActivity.this,DatosPersonales.class);
             startActivity(intent);
 
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_infoProfesional) {
             Intent intent = new Intent(MainActivity.this,editarInformacionProfesional.class);
             startActivity(intent);
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_postulaciones) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_empresa) {
 
         }
 
