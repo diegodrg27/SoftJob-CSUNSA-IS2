19d81190e2fae8c61cf27c7a248924157c1d8114
moebius <rcondorib@gmail.com>
2017-12-25 09:26:26 -0500

    Modulo Empresa
    Boton y mostrar

--- SoftJob/app/src/main/java/softjob/softjob/menuEmpresa.java
+++ SoftJob/app/src/main/java/softjob/softjob/menuEmpresa.java
@@ -7,14 +7,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
-
-public class menuEmpresa extends Fragment {
+public class menuEmpresa extends Fragment implements  View.OnClickListener  {
+    //crear objetos
+    Button btRegistrar;
+    TextView texto;
+    DatabaseReference database;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_empresa, container, false);
+        //btRegistrar =(Button) findViewById(R.id.btRegistrarEmpresa);
+        View v= inflater.inflate(R.layout.fragment_menu_empresa, container, false);
+        database= FirebaseDatabase.getInstance().getReference("Empresa");
+        btRegistrar =(Button)v.findViewById(R.id.btRegistrarEmpresa);
+        texto=(TextView)v.findViewById(R.id.txtEmpresa);
+        ///btRegistrar.setOnClickListener();
+        btRegistrar.setOnClickListener(this);
+        return  v;
+    }
+    public void onClick(View vista){
+        switch (vista.getId()){
+            case R.id.btRegistrarEmpresa:
+                texto.setText("Apretaste registrar empresa");
+        }
     }
 }
